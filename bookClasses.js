594a8b6845fb44492609a63bbc04e09957622858
becia.robert21@gmail.com <Vibevibe23!>
2022-06-21 11:04:53 +0300

    update test

--- bookClasses.js
+++ bookClasses.js
@@ -28,6 +28,28 @@ class Book {
     this.displayBook();
   }
 
+  displayBook = () => {
+    const bookCard = document.createElement('div');
+    const cardTitle = document.createElement('p');
+    const removeButton = document.createElement('button');
+
+    bookCard.classList.add('book-Card');
+
+    cardTitle.textContent = `"${this.title}"   by   ${this.author}`;
+    removeButton.textContent = 'Remove';
+    removeButton.classList.add('remove');
+
+    bookCard.appendChild(cardTitle);
+    bookCard.appendChild(removeButton);
+
+    booksContainer.appendChild(bookCard);
+
+    removeButton.addEventListener('click', () => {
+      this.removeBook();
+      Book.saveDataLocally();
+      booksContainer.removeChild(bookCard);
+    });
+  }
 }
 
 function checkRepetition(book) {
@@ -42,3 +64,63 @@ function checkRepetition(book) {
   return true;
 }
 
+function addBookToLibrary() {
+  const inputTitleValue = inputTitle.value;
+  const inputAuthorValue = inputAuthor.value;
+  const book = new Book(inputTitleValue.trim(), inputAuthorValue.trim());
+  if (checkRepetition(book)) {
+    book.addBook();
+    indicator.textContent = 'Book added succesfully';
+  }
+}
+
+const renderBooks = () => {
+  Book.collection.forEach((book) => {
+    book.displayBook();
+  });
+};
+
+const addForm = document.querySelector('form');
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  inputTitle.value = '';
+  inputAuthor.value = '';
+});
+
+window.onload = () => {
+  if (localStorage.getItem('library') !== null) {
+    const myStorageLibrary = JSON.parse(localStorage.getItem('library'));
+    myStorageLibrary.forEach((book) => {
+      const newBook = new Book(book.title, book.author);
+      Book.collection.push(newBook);
+    });
+  }
+  renderBooks();
+};
+
+const aTags = document.querySelectorAll('a');
+const sections = document.querySelectorAll('section');
+const reGex = /#[\w-]+/g;
+
+aTags.forEach((a) => {
+  a.addEventListener('click', (event) => {
+    const currentId = event.target.href.match(reGex)[0];
+    const currentSection = document.querySelector(currentId);
+    indicator.textContent = '';
+
+    let currentLink = document.querySelector('.selected');
+    currentLink.classList.remove('selected');
+
+    currentLink = event.target;
+
+    sections.forEach((section) => {
+      if (section === currentSection) {
+        section.classList.remove('d-none');
+        currentLink.classList.add('selected');
+      } else {
+        section.classList.add('d-none');
+      }
+    });
+  });
+});
